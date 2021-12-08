b509e9ab39741f1ce711aa137132912a52e34cd0
razzaksr <razzaksr@gmail.com>
2021-12-08 10:33:20 +0530

    Java Project created

---
+++ BadriJava/src/test/java/learn/console/BadriJava/AppTest.java
@@ -0,0 +1,20 @@
+package learn.console.BadriJava;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
