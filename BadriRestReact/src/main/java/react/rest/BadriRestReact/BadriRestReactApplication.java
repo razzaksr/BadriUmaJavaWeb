00112961841d59b86d14b186572f8f5e85c16281
razzaksr <razzaksr@gmail.com>
2022-05-01 12:39:11 +0530

    jar spring for react created

---
+++ BadriRestReact/src/main/java/react/rest/BadriRestReact/BadriRestReactApplication.java
@@ -0,0 +1,13 @@
+package react.rest.BadriRestReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadriRestReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadriRestReactApplication.class, args);
+	}
+
+}
---
+++ BadriRestReact/src/test/java/react/rest/BadriRestReact/BadriRestReactApplicationTests.java
@@ -0,0 +1,13 @@
+package react.rest.BadriRestReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadriRestReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
